f9b82bab438f1065c8486f49cfba9f32ea6a8764
IgorCerq22 <igor.cerqueira@jussi.com.br>
2025-03-25 16:42:54 -0300

    feat(backend): api tecnologia e reajuste do package

---
+++ backend/src/tecnologia/tecnologia.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common'
+import { Tecnologia } from '@core'
+import { TecnologiaProvider } from './tecnologia.provider'
+
+@Controller('tecnologias')
+export class TecnologiaController {
+	constructor(private readonly repo: TecnologiaProvider) {}
+
+	@Get()
+	async obterTodas(): Promise<Tecnologia[]> {
+		return this.repo.obterTodas()
+	}
+}
