c384a1a9f63cf80707deca0c1c075c4871f59393
IgorCerq22 <igor.cerqueira@jussi.com.br>
2025-03-25 16:46:02 -0300

    feat(backend): api tecnologia - obter destaques

--- backend/src/tecnologia/tecnologia.controller.ts
+++ backend/src/tecnologia/tecnologia.controller.ts
@@ -10,4 +10,9 @@ export class TecnologiaController {
 	async obterTodas(): Promise<Tecnologia[]> {
 		return this.repo.obterTodas()
 	}
+
+	@Get('destaques')
+	async obterDestaques(): Promise<Tecnologia[]> {
+		return this.repo.obterDestaques()
+	}
 }
